255de06401150f20d1c415de67b13640fe9480f2
732015168@qq.com <you10call5>
2018-03-02 09:41:58 +0800

    stock

--- header.py
+++
@@ -1,6 +0,0 @@
-/*
- * @Author: Dirk.Zhao 
- * @Date: 2018-03-01 16:41:00 
- * @Last Modified by:   Dirk.Zhao 
- * @Last Modified time: 2018-03-01 16:41:00 
- */
---
+++ stock/onestock.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/2 09:34
+# @Author  : Dirk Zhao
+import tushare as ts
+import pandas as pd
+
+
+ts.get_today_ticks(code='600760')
